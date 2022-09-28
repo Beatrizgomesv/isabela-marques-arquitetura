3f32ca7f2fd6ec2cdd40d012789f6c4a931bcf7e
Beatriz <beatrizg00@hotmail.com>
2022-09-27 21:22:06 -0300

    feat: adding the contact map

---
+++ src/components/Contact/Map/Map.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export default function Map() {
+  return (
+    <iframe
+      width="100%"
+      height="400"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3676.0216776527536!2d-47.2298577!3d-22.8756576!2m3!1f0!2f0!3f0!3m2
+    !1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8bb865f67bcf9%3A0xec3b00238c83e420!2sIsabela%20Marques%20Arquitetura%20e%20Interiores!5e0!3m2!1sen!2sbr!
+    4v1664320549176!5m2!1sen!2sbr"
+    ></iframe>
+  );
+}
